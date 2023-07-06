cb93a9ed33fbc272988b00dcf1e58ae0ce65e017
Koushik-Deb <koushik.imon@gmail.com>
2023-07-06 16:17:45 +0600

    initial setup 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montSerrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montSerrat.variable} font-mont`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
